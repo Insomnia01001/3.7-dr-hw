a685d0d7051ae5973f4d677b025283a1e4c44fc6
Insomnia01001 <167763964+Insomnia01001@users.noreply.github.com>
2024-10-31 20:39:16 +0300

    Add files via upload

---
+++ 3.7 dr hw/main.py
@@ -0,0 +1,20 @@
+# inp1 =input("enter your name:")
+# inp2 =input("enter your sername:")
+
+# def func1(ismlar,ism2):
+#     file1 = (open(file="students.txt",mode="w"))
+#     file1.write("familiya:  ")
+#     for i in ismlar:
+#         file1.write(i)
+#     file1.write("    ism:  ")
+#     for i in ism2:
+#         file1.write(i)
+#     file1.close()
+# func1(inp1,ism2=inp2)
+            
+# def get_names():
+#     file2 = open(file="students.txt",mode="r")
+#     a =file2.readlines()
+#     for i in a:
+#         print(i)
+# get_names()
